bca079d56431f11f3eec383177fb4059defcb5e2
John Jacquay <john@bioteam.net>
2020-02-12 08:54:30 -0700

    iRODS customizations

--- irods_file_system.cc
+++ irods_file_system.cc
@@ -1,17 +1,3 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
 #include "irods_file_system.h"
 
 #include <cstdlib>
@@ -24,18 +10,18 @@ limitations under the License.
 namespace tensorflow {
 
 namespace {
-static const char* kS3FileSystemAllocationTag = "S3FileSystemAllocation";
-static const size_t kS3ReadAppendableFileBufferSize = 1024 * 1024;
-static const int kS3GetChildrenMaxKeys = 100;
+static const char* iRODSFileSystemAllocationTag = "iRODSFileSystemAllocation";
+static const size_t iRODSReadAppendableFileBufferSize = 1024 * 1024;
+static const int iRODSGetChildrenMaxKeys = 100;
 
-class S3RandomAccessFile : public RandomAccessFile {
+class iRODSRandomAccessFile : public RandomAccessFile {
  public:
-  S3RandomAccessFile(const string& bucket, const string& object,
+  iRODSRandomAccessFile(const string& bucket, const string& object,
                      std::shared_ptr<void> s3_client)
       : bucket_(bucket), object_(object), s3_client_(s3_client) {}
 
   Status Name(StringPiece* result) const override {
-    return errors::Unimplemented("S3RandomAccessFile does not support Name()");
+    return errors::Unimplemented("iRODSRandomAccessFile does not support Name()");
   }
 
   Status Read(uint64 offset, size_t n, StringPiece* result,
@@ -77,68 +63,68 @@ class S3WritableFile : public WritableFile {
 
 }  // namespace
 
-S3FileSystem::S3FileSystem() {}
+iRODSFileSystem::iRODSFileSystem() {}
 
-S3FileSystem::~S3FileSystem() {}
+iRODSFileSystem::~iRODSFileSystem() {}
 
-Status S3FileSystem::NewRandomAccessFile(
+Status iRODSFileSystem::NewRandomAccessFile(
     const string& fname, std::unique_ptr<RandomAccessFile>* result) {
   return Status::OK();
 }
 
-Status S3FileSystem::NewWritableFile(const string& fname,
+Status iRODSFileSystem::NewWritableFile(const string& fname,
                                      std::unique_ptr<WritableFile>* result) {
   return Status::OK();
 }
 
-Status S3FileSystem::NewAppendableFile(const string& fname,
+Status iRODSFileSystem::NewAppendableFile(const string& fname,
                                        std::unique_ptr<WritableFile>* result) {
   return Status::OK();
 }
 
-Status S3FileSystem::NewReadOnlyMemoryRegionFromFile(
+Status iRODSFileSystem::NewReadOnlyMemoryRegionFromFile(
     const string& fname, std::unique_ptr<ReadOnlyMemoryRegion>* result) {
   return Status::OK();
 }
 
-Status S3FileSystem::FileExists(const string& fname) {
+Status iRODSFileSystem::FileExists(const string& fname) {
   return Status::OK();
 }
 
-Status S3FileSystem::GetChildren(const string& dir,
+Status iRODSFileSystem::GetChildren(const string& dir,
                                  std::vector<string>* result) {
   return Status::OK();
 }
 
-Status S3FileSystem::Stat(const string& fname, FileStatistics* stats) {
+Status iRODSFileSystem::Stat(const string& fname, FileStatistics* stats) {
   return Status::OK();
 }
 
-Status S3FileSystem::GetMatchingPaths(const string& pattern,
+Status iRODSFileSystem::GetMatchingPaths(const string& pattern,
                                       std::vector<string>* results) {
   return Status::OK();
 }
 
-Status S3FileSystem::DeleteFile(const string& fname) {
+Status iRODSFileSystem::DeleteFile(const string& fname) {
   return Status::OK();
 }
 
-Status S3FileSystem::CreateDir(const string& dirname) {
+Status iRODSFileSystem::CreateDir(const string& dirname) {
   return Status::OK();
 }
 
-Status S3FileSystem::DeleteDir(const string& dirname) {
+Status iRODSFileSystem::DeleteDir(const string& dirname) {
   return Status::OK();
 }
 
-Status S3FileSystem::GetFileSize(const string& fname, uint64* file_size) {
+Status iRODSFileSystem::GetFileSize(const string& fname, uint64* file_size) {
   return Status::OK();
 }
 
-Status S3FileSystem::RenameFile(const string& src, const string& target) {
+Status iRODSFileSystem::RenameFile(const string& src, const string& target) {
   return Status::OK();
 }
 
-REGISTER_FILE_SYSTEM("s3", S3FileSystem);
+REGISTER_FILE_SYSTEM("irods", iRODSFileSystem);
 
 }  // namespace tensorflow
--- irods_file_system.h
+++ irods_file_system.h
@@ -1,18 +1,3 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 #ifndef TENSORFLOW_CONTRIB_S3_S3_FILE_SYSTEM_H_
 #define TENSORFLOW_CONTRIB_S3_S3_FILE_SYSTEM_H_
 
@@ -21,10 +6,10 @@ limitations under the License.
 
 namespace tensorflow {
 
-class S3FileSystem : public FileSystem {
+class iRODSFileSystem : public FileSystem {
  public:
-  S3FileSystem();
-  ~S3FileSystem();
+  iRODSFileSystem();
+  ~iRODSFileSystem();
 
   Status NewRandomAccessFile(
       const string& fname, std::unique_ptr<RandomAccessFile>* result) override;
